c29494de30f8f648cb6148ad3c49c9d27c562bcc
user <wow@cmail.com>
2018-12-29 20:48:55 +0800

    update music

--- commit-music.bat
+++ commit-music.bat
@@ -1,5 +1,11 @@
 @echo off
 echo %~dp0
+set rsa_path=ignore/pack/ssh-key/id_rsa
+git config --global user.email "wow@cmail.com"
+git config --global user.name "user"
+git remote remove origin
+git remote add origin git@github.com:BetterWOW/music-noon.git
+git config core.sshCommand "ssh -i %rsa_path%"
 git add .
 git commit -m "update music"
 git push -u origin master
