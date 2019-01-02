aa3c8101b7ea3a773c1162598232cd5f524feb13
user <wow@cmail.com>
2019-01-02 09:07:19 +0800

    update music

--- music/~play.bat
+++ music/~play.bat
@@ -1,5 +1,7 @@
 @echo off
-for  %%i in (*) do set muisicName=%%i
+::for  %%i in (*) do 
+cd /d %~dp0
+set muisicName=Àî¾ÁÕÜ - ÏëÌ«¶à.mp3
 echo "start play----%muisicName%"
 call "%muisicName%"
 pause
