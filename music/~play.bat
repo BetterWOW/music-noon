1ba2a19b85f130080b294d6aec428f3227aee29d
user <wow@cmail.com>
2019-01-02 09:36:31 +0800

    update music

--- music/~play.bat
+++ music/~play.bat
@@ -2,9 +2,6 @@
 set muisicName=Àî¾ÁÕÜ - ÏëÌ«¶à.mp3
 ::for  %%i in (*) do 
 cd /d %~dp0
-cd ..
-git pull origin master --depth=1
-cd /d %~dp0
 echo "start play----%muisicName%"
 call "%muisicName%"
 pause
