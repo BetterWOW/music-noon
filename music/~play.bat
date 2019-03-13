a13842fc8cca0a053fc4d65bbc0398729f76d011
user <wow@cmail.com>
2019-03-13 22:18:28 +0800

    update music

--- music/~play.bat
+++ music/~play.bat
@@ -1,12 +1,21 @@
-::@echo off
+@echo off
 set muisicName=李玖哲 - 想太多.mp3
 ::for  %%i in (*) do
-
-
 ::cd到本脚本所在的路径
 cd /d %~dp0
-
+set dirPath=%~dp0
 
 echo "start play----%muisicName%"
 call "%muisicName%"
-pause
+
+::播放后关闭当前播放定时任务并启动pull-music定时任务
+schtasks /Delete /tn play-music /F>nul
+::pull-music-task (13:30-13:59)
+set pull_music_task_bat_path=%dirPath%~pull-music.bat
+schtasks  /create /tn "pull-music-task" /sc WEEKLY /d MON,TUE,WED,THU,FRI /st 13:30 /et 13:50 /tr "%pull_music_task_bat_path%"
+
+
+echo "finished"
+echo "shutdown console in 10s"
+ping -n 11 127.0.0.1>nul
+::pause
--- music/~pull-music.bat
+++ music/~pull-music.bat
@@ -1,11 +1,20 @@
-set rsa_path=.git/ignore/music-noon/ssh-key/id_rsa
+@echo off
 cd /d %~dp0
 set dirPath=%~dp0
-set pull_music_task_bat_path=%dirPath%~pull-music.bat
-echo %pull_music_task_bat_path%
-cd ..
+
+::pull 之前创建播放定时任务并关闭pull-music定时任务
+::play-music-task (14:00-14:11)
+set play_music_task_bat_path=%dirPath%~play.bat
+schtasks  /create /tn "play-music" /sc WEEKLY /d MON,TUE,WED,THU,FRI /st 14:00 /et 14:11 /tr "%play_music_task_bat_path%" /F
+schtasks /Delete /tn pull-music-task /F>nul
+
+::开始pull
+set rsa_path=.git/ignore/music-noon/ssh-key/id_rsa
 git remote remove origin
 git remote add origin git@github.com:BetterWOW/music-noon.git
 git config core.sshCommand "ssh -i %rsa_path%"
 git pull origin master --depth=1
-pause
+
+echo "finished"
+echo "shutdown console in 10s"
+ping -n 11 127.0.0.1>nul
